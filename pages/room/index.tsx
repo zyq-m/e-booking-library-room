6df92c405c70761698584d3900993d37d9e4a2d5
Haziq Musa <last.game01987@gmail.com>
2022-12-13 01:40:03 +0800

    filtered function

--- pages/room/index.tsx
+++ pages/room/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import Rooms from "../../components/Rooms";
 import { TRoom } from "../../components/RoomCard";
 import Sort from "../../components/Sort";
@@ -6,6 +6,7 @@ import Sort from "../../components/Sort";
 const dummyData: TRoom[] = [
   {
     name: "karel room 1",
+    roomId: "room1",
     capacity: 1,
     image:
       "https://i.pinimg.com/originals/1e/e8/87/1ee887d7cfcbd959e386fee478adb0ec.jpg",
@@ -13,6 +14,7 @@ const dummyData: TRoom[] = [
   },
   {
     name: "karel room 2",
+    roomId: "room2",
     capacity: 1,
     image:
       "https://i.pinimg.com/originals/1e/e8/87/1ee887d7cfcbd959e386fee478adb0ec.jpg",
@@ -20,6 +22,7 @@ const dummyData: TRoom[] = [
   },
   {
     name: "Meeting room",
+    roomId: "room3",
     capacity: 5,
     image:
       "https://gurney.ghotel.com.my/img/asset/bWFpbi9pbWFnZXMvbWVldGluZ3MvY29uZmVyZW5jZS1pLS0taWktMDEtdjUuanBn?w=1920&fit=crop&s=027ab1a7a42b5feb2a3183fe3e952455",
@@ -28,11 +31,30 @@ const dummyData: TRoom[] = [
 ];
 
 export default function Room() {
+  const [statusRoom, setStatusRoom] = useState<boolean | string>("all");
+  const [rooms, setRooms] = useState<TRoom[]>([]);
+
+  const filteredRoom = useMemo(getFilteredRoom, [rooms, statusRoom]);
+
+  function getFilteredRoom() {
+    if (statusRoom === "all") {
+      return rooms;
+    }
+    return rooms.filter(room => room.isAvailable === statusRoom);
+  }
+
+  React.useEffect(() => {
+    setRooms(dummyData);
+  }, []);
+
   return (
     <div>
       <h2 className="mb-6 text-center text-lg">Searching Room</h2>
-      <Sort totalRoom={dummyData.length} />
-      <Rooms list={dummyData} />
+      <Sort
+        totalRoom={filteredRoom.length}
+        roomStatus={status => setStatusRoom(status)}
+      />
+      <Rooms list={filteredRoom} />
     </div>
   );
 }
