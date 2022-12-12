1d31ce9967315849bf49733809fefeaba11e543b
Haziq Musa <last.game01987@gmail.com>
2022-12-13 01:48:44 +0800

    split data

--- pages/room/index.tsx
+++ pages/room/index.tsx
@@ -1,34 +1,8 @@
 import React, { useState, useMemo } from "react";
 import Rooms from "../../components/Rooms";
-import { TRoom } from "../../components/RoomCard";
 import Sort from "../../components/Sort";
-
-const dummyData: TRoom[] = [
-  {
-    name: "karel room 1",
-    roomId: "room1",
-    capacity: 1,
-    image:
-      "https://i.pinimg.com/originals/1e/e8/87/1ee887d7cfcbd959e386fee478adb0ec.jpg",
-    isAvailable: true,
-  },
-  {
-    name: "karel room 2",
-    roomId: "room2",
-    capacity: 1,
-    image:
-      "https://i.pinimg.com/originals/1e/e8/87/1ee887d7cfcbd959e386fee478adb0ec.jpg",
-    isAvailable: false,
-  },
-  {
-    name: "Meeting room",
-    roomId: "room3",
-    capacity: 5,
-    image:
-      "https://gurney.ghotel.com.my/img/asset/bWFpbi9pbWFnZXMvbWVldGluZ3MvY29uZmVyZW5jZS1pLS0taWktMDEtdjUuanBn?w=1920&fit=crop&s=027ab1a7a42b5feb2a3183fe3e952455",
-    isAvailable: true,
-  },
-];
+import data from "../../public/data";
+import { TRoom } from "../../components/RoomCard";
 
 export default function Room() {
   const [statusRoom, setStatusRoom] = useState<boolean | string>("all");
@@ -44,7 +18,7 @@ export default function Room() {
   }
 
   React.useEffect(() => {
-    setRooms(dummyData);
+    setRooms(data);
   }, []);
 
   return (
---
+++ public/data.ts
@@ -0,0 +1,30 @@
+import { TRoom } from "../components/RoomCard";
+
+const data: TRoom[] = [
+  {
+    name: "karel room 1",
+    roomId: "room1",
+    capacity: 1,
+    image:
+      "https://i.pinimg.com/originals/1e/e8/87/1ee887d7cfcbd959e386fee478adb0ec.jpg",
+    isAvailable: true,
+  },
+  {
+    name: "karel room 2",
+    roomId: "room2",
+    capacity: 1,
+    image:
+      "https://i.pinimg.com/originals/1e/e8/87/1ee887d7cfcbd959e386fee478adb0ec.jpg",
+    isAvailable: false,
+  },
+  {
+    name: "Meeting room",
+    roomId: "room3",
+    capacity: 5,
+    image:
+      "https://gurney.ghotel.com.my/img/asset/bWFpbi9pbWFnZXMvbWVldGluZ3MvY29uZmVyZW5jZS1pLS0taWktMDEtdjUuanBn?w=1920&fit=crop&s=027ab1a7a42b5feb2a3183fe3e952455",
+    isAvailable: true,
+  },
+];
+
+export default data;
