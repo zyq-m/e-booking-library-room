bec9a94034c73a2ccd4b29fe3444b8e669aeda57
Haziq Musa <last.game01987@gmail.com>
2022-12-18 23:04:27 +0800

    new: page

---
+++ pages/home.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import Carousel from "../components/Carousel";
+import Search from "../components/Search";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>
+        <h2 className="text-xl font-medium">Hi Alex, Let{"'"}s</h2>
+        <Search />
+        <section>
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-lg">Our rooms</h2>
+            <Link href="#" className="text-xs lg:text-sm">
+              See more
+            </Link>
+          </div>
+          <Carousel />
+        </section>
+      </div>
+    </Layout>
+  );
+}
