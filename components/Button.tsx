7eb655da76fa85d71612bf72f2e0e01b697c996c
Haziq Musa <last.game01987@gmail.com>
2022-12-19 13:35:13 +0800

    update: component

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,13 +4,15 @@ type TButon = {
   styles?: string;
   label: string;
   disable?: boolean;
+  onClick?: () => void;
 };
 
-export default function Button({ styles, label, disable }: TButon) {
+export default function Button({ styles, label, disable, onClick }: TButon) {
   return (
     <button
       type="button"
       className={`${styles} p-2 rounded-3xl bg-blue-400 text-white`}
+      onClick={onClick}
       disabled={disable}>
       {label}
     </button>
