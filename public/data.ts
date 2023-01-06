2b841dce6186ad521ee6ab39821007f54ff0a966
Haziq Musa <last.game01987@gmail.com>
2023-01-06 19:39:59 +0800

    new dummy data

--- public/data.ts
+++ public/data.ts
@@ -27,4 +27,32 @@ const data: TRoom[] = [
   },
 ];
 
+type THistory = TRoom & {
+  date: string;
+  duration: number;
+};
+
+export const bookHistory: THistory[] = [
+  {
+    name: "Meeting room",
+    roomId: "room3",
+    capacity: 5,
+    image:
+      "https://gurney.ghotel.com.my/img/asset/bWFpbi9pbWFnZXMvbWVldGluZ3MvY29uZmVyZW5jZS1pLS0taWktMDEtdjUuanBn?w=1920&fit=crop&s=027ab1a7a42b5feb2a3183fe3e952455",
+    isAvailable: true,
+    date: "06-10-2023",
+    duration: 3,
+  },
+  {
+    name: "Karel room 1",
+    roomId: "room1",
+    capacity: 1,
+    image:
+      "https://gurney.ghotel.com.my/img/asset/bWFpbi9pbWFnZXMvbWVldGluZ3MvY29uZmVyZW5jZS1pLS0taWktMDEtdjUuanBn?w=1920&fit=crop&s=027ab1a7a42b5feb2a3183fe3e952455",
+    isAvailable: true,
+    date: "09-10-2023",
+    duration: 2,
+  },
+];
+
 export default data;
