99a001a748293d23b14356f578f538230297b0cf
Haziq Musa <last.game01987@gmail.com>
2023-01-09 11:01:12 +0800

    new component

---
+++ components/BookingHistoryList.tsx
@@ -0,0 +1,29 @@
+import React, { memo } from "react";
+
+export type THistory = {
+  name: string;
+  date: string;
+};
+
+type THList = {
+  list: THistory[];
+};
+
+function BookingHistoryList({ list }: THList) {
+  return (
+    <ul id="recent" className="mt-2 text-gray-400 text">
+      {list.map((data, i) => {
+        return (
+          <li
+            key={i}
+            className="flex justify-between items-center py-2 px-3 rounded-md hover:bg-gray-100">
+            <p>{data.name}</p>
+            <p className="text-sm">{data.date}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default memo(BookingHistoryList);
---
+++ components/BookingStatus.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function BookingStatus() {
+  return (
+    <div id="stat" className="py-3 px-4 border rounded-xl text-sm mt-4 mb-6">
+      <p>Karel Room 1</p>
+      <p className="text-2xl font-medium text-red-400 mb-2">2 hours left</p>
+      <p className="text-gray-500">6 Jan 2023</p>
+    </div>
+  );
+}
