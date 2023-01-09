c7801b922d6a2c927285e0047c48fdd775f870b2
Haziq Musa <last.game01987@gmail.com>
2023-01-09 11:01:58 +0800

    update style

--- pages/home.tsx
+++ pages/home.tsx
@@ -10,10 +10,10 @@ export default function Home({ data }: { data: TRoom[] }) {
   return (
     <Layout>
       <div>
-        <h2 className="text-xl font-medium">Hi Alex, Let{"'"}s</h2>
+        <h2 className="text-xl font-medium ml-1">Hi Alex, Let{"'"}s</h2>
         <Search />
         <section>
-          <div className="flex justify-between items-center mb-4 px-1">
+          <div className="flex justify-between items-center mb-4 mx-1">
             <h2 className="text-lg">Our rooms</h2>
             <Link href="/room/all" className="text-xs lg:text-sm">
               See more
