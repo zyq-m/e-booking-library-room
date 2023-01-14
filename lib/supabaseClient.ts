cb67e5a69e0732b628387a646137ea577a878a35
Haziq Musa <last.game01987@gmail.com>
2023-01-15 07:51:20 +0800

    deleted

--- lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
-
-export default function useSupabase() {
-  const session = useSession();
-  const supabase = useSupabaseClient();
-
-  return { session, supabase };
-}
