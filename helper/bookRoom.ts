098e3297fca7717e868151288bad36d1a29b50c5
Haziq Musa <last.game01987@gmail.com>
2023-01-16 10:46:54 +0800

    create helper

--- helper/bookRoom.ts
+++ helper/bookRoom.ts
@@ -1,15 +1,20 @@
 import { supabase } from "../lib/supabase";
+import { fetchUserId } from "./fetchUserId";
 
 export async function bookRoom(
   token: string | undefined,
-  from: string,
-  to: string,
+  from: string | undefined,
+  to: string | undefined,
   roomId: string
 ) {
-  const id = (await supabase.auth.getUser(token)).data.user?.id;
-  const { data, error } = await supabase
+  const id = await fetchUserId(token);
+  const booking = supabase
     .from("booking")
-    .insert({ id, from, to, roomId });
+    .insert({ userId: id, from: from, to: to, roomId: roomId }); // add record to booking table
+  const update = supabase
+    .from("room")
+    .update({ isBooked: true, isAvailable: false })
+    .eq("roomId", roomId); // update booked and available status
 
-  return { data, error };
+  return await Promise.all([booking, update]);
 }
