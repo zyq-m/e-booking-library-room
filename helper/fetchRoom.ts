849fa081bddd5b9275c3e91ec808a330e01ef5a6
Haziq Musa <last.game01987@gmail.com>
2023-01-16 12:24:35 +0800

    update

--- helper/fetchRoom.ts
+++ helper/fetchRoom.ts
@@ -1,4 +1,6 @@
 import { supabase } from "../lib/supabase";
+import { fetchUserId } from "./fetchUserId";
+import moment from "moment";
 
 export async function fetchRooms() {
   const { data, error } = await supabase.from("room").select("*");
@@ -11,6 +13,42 @@ export async function fetchRoomByName(name: string | string[] | undefined) {
     .from("room")
     .select()
     .like("name", `%${name}%`);
+
+  return { data, error };
+}
+
+export async function fetchCurrentBookedRoom(token: string | undefined) {
+  const date = moment().format("YYYY-MM-DD");
+  const userId = await fetchUserId(token);
+  const { data, error } = await supabase
+    .from("booking")
+    .select(
+      `room(
+        name,capacity,isBooked,image,isAvailable,roomId
+      )`
+    )
+    .eq("userId", userId)
+    .eq("date", date)
+    .eq("room.isBooked", true)
+    .order("from", { ascending: false });
+
+  return { data, error };
+}
+
+export async function fetchHistoryBooked(token: string | undefined) {
+  const userId = await fetchUserId(token);
+  const { data, error } = await supabase
+    .from("booking")
+    .select(
+      `
+      date,
+      from,
+      to,
+      room (
+        name
+      )`
+    )
+    .eq("userId", userId);
 
   return { data, error };
 }
