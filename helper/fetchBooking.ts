4eab0043e91c01e80ad5f8d0d82409d639269ea7
Haziq Musa <last.game01987@gmail.com>
2023-01-17 02:40:50 +0800

    create helper

---
+++ helper/fetchBooking.ts
@@ -0,0 +1,17 @@
+import { supabase } from "../lib/supabase";
+
+export async function fetchNotApprovedRoom() {
+  const booking = await supabase.from("booking").select(`
+        user(
+            name
+        ),
+        room(
+            name
+        ),
+        from,
+        to,
+        date
+    `);
+
+  return booking;
+}
