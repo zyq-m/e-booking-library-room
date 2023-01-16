7c718aabf5e2a504727d114902b35e39a4baceb0
Haziq Musa <last.game01987@gmail.com>
2023-01-16 09:27:27 +0800

    create serverSideProp func

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import Layout from "../components/Layout";
 import Carousel from "../components/Carousel";
 import Search from "../components/Search";
-import data from "../public/data";
 import { TRoom } from "../components/RoomCard";
 
-export default function Home({ data }: { data: TRoom[] }) {
+import { fetchUser } from "../helper/fetchUser";
+import { fetchRooms } from "../helper/fetchRoom";
+import { GetServerSidePropsContext } from "next";
+
+interface Data {
+  data: {
+    data: TRoom[];
+    name: string;
+  };
+}
+
+export default function Home({ data }: Data) {
   return (
     <Layout>
       <div>
-        <h2 className="text-xl font-medium ml-1">Hi Alex, Let{"'"}s</h2>
+        <h2 className="text-xl font-medium ml-1">
+          Hi {data.name}, Let{"'"}s
+        </h2>
         <Search />
         <section>
           <div className="flex justify-between items-center mb-4 mx-1">
@@ -19,13 +31,28 @@ export default function Home({ data }: { data: TRoom[] }) {
               See more
             </Link>
           </div>
-          <Carousel data={data} />
+          <Carousel data={data.data} />
         </section>
       </div>
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
-  return { props: { data: data } };
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const token = JSON.parse(
+    context.req.cookies?.["supabase-auth-token"] || ""
+  )[0];
+  const user = fetchUser(token);
+  const rooms = fetchRooms();
+
+  try {
+    const response = await Promise.all([user, rooms]);
+
+    const name = response[0].data?.[0]?.name;
+    const roomData = response[1]?.data;
+
+    return { props: { data: { data: roomData, name: name } } };
+  } catch (error) {
+    console.log(error);
+  }
 }
