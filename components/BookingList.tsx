46735498b50a0bd2eebdb1e430c35aabbbdbd361
Haziq Musa <last.game01987@gmail.com>
2023-01-17 03:57:44 +0800

    refactor

--- components/BookingList.tsx
+++ components/BookingList.tsx
@@ -28,59 +28,64 @@ function BookingList({ list, checkbox, approvedList }: TRecordsList) {
 
   useEffect(() => {
     approvedList?.(booking);
+    return () => approvedList?.([]);
   }, [approvedList, booking]);
 
   return (
-    <table className="w-full overflow-scroll text-sm">
-      <thead>
-        <tr className="border-y text-gray-400 sticky top-0 bg-gray-50">
-          {checkbox && <th></th>}
-          <th
-            className={`font-normal uppercase text-xs text-left py-2 ${
-              !checkbox && `pl-2`
-            }`}>
-            User
-          </th>
-          <th className="px-8 font-normal uppercase text-xs text-right">
-            Room
-          </th>
-          <th className="font-normal uppercase text-xs text-center">Time</th>
-          <th className="font-normal uppercase text-xs text-right pr-3">
-            Duration
-          </th>
-          <th className="font-normal uppercase text-xs text-left pr-2">Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {list.map(li => {
-          let from = moment(`${li.from}`, "HH:mm");
-          let to = moment(`${li.to}`, "HH:mm");
+    <div className=" overflow-x-auto">
+      <table className="w-full min-w-[26rem] text-sm">
+        <thead>
+          <tr className="border-y text-gray-400 sticky top-0 bg-gray-50">
+            {checkbox && <th></th>}
+            <th
+              className={`font-normal uppercase text-xs text-left py-2 ${
+                !checkbox && `pl-2`
+              }`}>
+              User
+            </th>
+            <th className="px-8 font-normal uppercase text-xs text-right">
+              Room
+            </th>
+            <th className="font-normal uppercase text-xs text-center">Time</th>
+            <th className="font-normal uppercase text-xs text-right pr-3">
+              Duration
+            </th>
+            <th className="font-normal uppercase text-xs text-left pr-2">
+              Date
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map(li => {
+            let from = moment(`${li.from}`, "HH:mm");
+            let to = moment(`${li.to}`, "HH:mm");
 
-          return (
-            <tr key={li.bookingId} className="border-y">
-              {checkbox && (
-                <td>
-                  <input
-                    type="checkbox"
-                    className="ml-2"
-                    onChange={e => onChange(li.bookingId, e)}
-                  />
+            return (
+              <tr key={li.bookingId} className="border-y">
+                {checkbox && (
+                  <td>
+                    <input
+                      type="checkbox"
+                      className="ml-2"
+                      onChange={e => onChange(li.bookingId, e)}
+                    />
+                  </td>
+                )}
+                <td className={`text-left py-4 ${!checkbox && `pl-2`}`}>
+                  {li.user.name}
                 </td>
-              )}
-              <td className={`text-left py-4 ${!checkbox && `pl-2`}`}>
-                {li.user.name}
-              </td>
-              <td className="text-right px-8">{li.room.name}</td>
-              <td className="text-center">{from.format("HH:mm")}</td>
-              <td className="text-right pr-3">
-                {to.diff(from, "hours")} hours
-              </td>
-              <td className="text-left pr-2 text-gray-400">{li.date}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                <td className="text-right px-8">{li.room.name}</td>
+                <td className="text-center">{from.format("HH:mm")}</td>
+                <td className="text-right pr-3">
+                  {to.diff(from, "hours")} hours
+                </td>
+                <td className="text-left pr-2 text-gray-400">{li.date}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
