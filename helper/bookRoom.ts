3c0613f976097457bc76c6b7347adbc621803e18
Haziq Musa <last.game01987@gmail.com>
2023-01-25 08:50:37 +0800

    refactor & create new api call

--- helper/bookRoom.ts
+++ helper/bookRoom.ts
@@ -1,5 +1,18 @@
 import { SupabaseClient } from "@supabase/supabase-js";
+import moment from "moment";
 import { fetchUserId } from "./fetchUserId";
+
+async function checkTodayBooking(supabase: SupabaseClient, id?: string) {
+  const { data } = await supabase
+    .from("booking")
+    .select("bookingId")
+    .eq("userId", id)
+    .eq("date", moment().format("YYYY-MM-DD"));
+
+  if (data?.some(id => data?.[0].bookingId == id.bookingId)) {
+    return Promise.reject("You only can book once a day");
+  }
+}
 
 export async function bookRoom(
   supabase: SupabaseClient,
@@ -16,6 +29,30 @@ export async function bookRoom(
     .update({ isBooked: true, isAvailable: false })
     .eq("roomId", roomId); // update booked and available status
 
+  await checkTodayBooking(supabase, id);
+  return await Promise.all([booking, update]);
+}
+
+export async function bookRoomPermission(
+  supabase: SupabaseClient,
+  from: string | undefined,
+  to: string | undefined,
+  roomId: string
+) {
+  const id = await fetchUserId(supabase);
+  const booking = supabase.from("booking").insert({
+    userId: id,
+    from: from,
+    to: to,
+    roomId: roomId,
+    approved: false,
+  }); // add record to booking table
+  const update = supabase
+    .from("room")
+    .update({ isBooked: true, isAvailable: false })
+    .eq("roomId", roomId); // update booked and available status
+
+  await checkTodayBooking(supabase, id);
   return await Promise.all([booking, update]);
 }
 
